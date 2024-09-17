251179176716f4f72c37f787395bb0020fcfa14a
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-17 17:44:37 +0530

    add comments

--- java samples/src/basics/HelloWorld.java
+++ java samples/src/basics/HelloWorld.java
@@ -3,5 +3,6 @@ package basics;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to Java with Vscode");
+        // System.out.println("this is a another line of code"); // single line comment
     }
 }
