630a50d80b368884ff77004a475a0fba65969377
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-17 19:07:39 +0530

    add datatypes

---
+++ java samples/src/basics/DataTypes.java
@@ -0,0 +1,59 @@
+package basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        /*
+         * Primitve Types
+         * byte
+         * short
+         * int
+         * long
+         * float
+         * double
+         * char
+         * boolean
+         */
+        /*
+         * object types
+         * String
+         * Array
+         * Byte
+         * Short
+         * Int
+         * Long
+         * Float
+         * Double
+         * Character
+         * Boolean
+         * ---- many more built-in and custom object can also be present
+         */
+
+        /*
+         * Size allocation for each type
+         * byte -- 8 bit signed
+         * short -- 16 bit signed
+         * int -- 32 bit signed
+         * long -- 64 bit signed
+         * float -- 32 bit signed
+         * double -- 64 bit signed
+         * char -- 16 bit
+         * boolean -- 1 bit
+         */
+
+        // usage
+        byte b = 10;
+        short s = 100;
+        int i = 1000;
+        long l = 10000;
+        float f = 100.25f;
+        double d = 1000.75;
+
+        System.out.println("The value of byte variable is " + b);
+        System.out.println("The value of short variable is " + s);
+        System.out.println("The value of int variable is " + i);
+        System.out.println("The value of long variable is " + l);
+        System.out.println("The value of float variable is " + f);
+        System.out.println("The value of double variable is " + d);
+
+    }
+}
