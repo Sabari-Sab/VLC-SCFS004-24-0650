f95de28f99fe56af770ea85c5fa61f18e655d497
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-17 18:52:19 +0530

    add more type declarations

--- java samples/src/basics/VariableDeclaration.java
+++ java samples/src/basics/VariableDeclaration.java
@@ -16,5 +16,24 @@ public class VariableDeclaration {
         // declare integer type variable
         int age; // variable declared without initialization
         int sal = 1000; // variable declared without initialization
+
+        // declare floating point variables
+        double amount; // variable declared without initialization
+        double price = 56.25; // variable declared without initialization
+
+        // character type variables
+        char initial; // variable declared without initialization
+        char init = 'a'; // variable declared without initialization
+
+        // boolean type variable
+        boolean isValid = true;
+
+        // String type variable
+        String name;
+        String firstName = "Alex";
+
+        // delare multiple variables in a single line
+        int x = 10, y = 20, z;
+
     }
 }
