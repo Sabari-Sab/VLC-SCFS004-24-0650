9c4c9d43ca1aaa84e8260e270eb500f417ee166e
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-17 18:36:15 +0530

    add variable declaration

---
+++ java samples/src/basics/VariableDeclaration.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        /*
+         * declaring a variable involves specifying the data type followed by the
+         * variable name.
+         * You can also optionally assign a value to the variable at the time of
+         * declaration.
+         * Syntax
+         * data_type variable_name;
+         * data_type variable_name = value;
+         * 
+         * 
+         */
+    }
+}
