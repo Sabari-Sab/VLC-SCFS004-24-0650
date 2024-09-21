62d6ee0fb0cc6e0e8d2870acea3e61f23380f75d
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-21 13:10:58 +0530

    add if condition

---
+++ java samples/src/loops/ConditionsIf.java
@@ -0,0 +1,32 @@
+package loops;
+
+public class ConditionsIf {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        /*
+         * in any of the blocks (if/else if / else) if there is more than one line of
+         * executable code use the {}
+         */
+
+        // basic syntax of a if block
+        if (x > y) {
+            System.out.println("good");
+        }
+
+        // else if block (there can be more than one else if block)
+        if (x > y) {
+            System.out.println("good");
+        } else if (x < y) {
+            System.out.println("bad");
+        }
+
+        // complete syntax of the if block including the else
+        if (x == y) {
+            System.out.println("good");
+        } else if (x > y) {
+            System.out.println("bad");
+        } else
+            System.out.println("ugly");
+
+    }
+}
