0b14286381f8b1e096d1ba0249cc4c58282e3f5f
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-21 09:25:27 +0530

    add user input

--- java samples/src/basics/UserInput.java
+++ java samples/src/basics/UserInput.java
@@ -5,10 +5,22 @@ import java.util.Scanner;
 public class UserInput {
     public static void main(String[] args) {
         /* Scanner class which is in the java.util package */
-        String name;
+        String name, prof;
+        byte age;
+        int sal;
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter your name: ");
-        name = scan.nextLine();
-        System.out.println("Welcome! " + name);
+        name = scan.nextLine(); // scanning a line of text
+        System.out.print("Enter your age: ");
+        age = scan.nextByte(); // scanning for byte type
+        System.out.print("Enter your Salary: ");
+        sal = scan.nextInt(); // scanning for int type
+        System.out.print("Enter your Profession: ");
+        prof = scan.next(); // scanning for String type (one word)
+
+        System.out.println("Welcome! " + name + " you are " + age + " years old " + " and earn a salary of " + sal
+                + " working as " + prof);
+
+        scan.close();
     }
 }
