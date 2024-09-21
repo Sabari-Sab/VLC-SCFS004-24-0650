213174e8ba6c053a1d149cd8412632edc0d34801
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-21 13:38:14 +0530

    add for each

---
+++ java samples/src/Arrays/Loops.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Loops {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 55, 33, 33 };
+        for (int i : nums) {
+            System.out.println(i);
+        }
+    }
+}
