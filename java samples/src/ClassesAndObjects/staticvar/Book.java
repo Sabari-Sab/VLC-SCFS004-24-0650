c8b5bdb33d8f0ae86ac26d6f43b6ddd0225e5da5
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-24 17:30:55 +0530

    add static var

---
+++ java samples/src/ClassesAndObjects/staticvar/Book.java
@@ -0,0 +1,13 @@
+package classesandobjects.staticvar;
+
+public class Book {
+    // instance variables or attributes or fields
+    String title;
+    String author;
+    int PublicationYear;
+    String genre;
+    int numberOfPages;
+
+    // static variable (class variable)
+    static String Library = "National Library"; // the value is shared across all instances
+}
