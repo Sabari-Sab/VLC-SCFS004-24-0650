86ebf3c9a473e9a0156b783068792315bfb657b5
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-24 16:55:18 +0530

    add classes

---
+++ java samples/src/ClassesAndObjects/Book.java
@@ -0,0 +1,12 @@
+package ClassesAndObjects;
+
+// Creating a blueprint of a book
+// Main method not needed
+public class Book {
+    // instance variables or attributes or fields
+    String title;
+    String author;
+    int PublicationYear;
+    String genre;
+    int numberOfPages;
+}
