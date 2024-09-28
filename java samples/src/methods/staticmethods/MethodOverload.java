84fee8d3419c2ed87c007f2d348f6a3737de0e27
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-28 08:52:21 +0530

    add overload

--- java samples/src/methods/staticmethods/MethodOverload.java
+++ java samples/src/methods/staticmethods/MethodOverload.java
@@ -2,21 +2,40 @@ package methods.staticmethods;
 
 public class MethodOverload {
 
-    public static int add(int x, int y) {
+    public static int add(int x, int y) { // original method
         return x + y;
     }
 
-    public static int add(int x, int y, int z) {
-        return x + y + z;
+    public static int add(int a, int b, int c) { // increased the # of parameters
+        return a + b + c;
     }
 
-    public static String add(String x, String y) {
+    public static int add(int... x) { // use of variable arguments
+        int res = 0;
+        for (int i : x) {
+            res += i;
+        }
+        return res;
+    }
+
+    public static double add(double... x) { // use of variable arguments
+        double res = 0;
+        for (double i : x) {
+            res += i;
+        }
+        return res;
+    }
+
+    public static String add(String x, String y) { // changed the types of parameters
         return x + y;
     }
 
     public static void main(String[] args) {
         System.out.println(add(10, 20));
         System.out.println(add(10, 20, 30));
+        System.out.println(add(10, 20, 30, 40, 50));
+        System.out.println(add(10, 20, 30, 40, 50, 60, 70));
+        System.out.println(add(10.4, 20, 30.5, 40, 50.44, 60, 70));
         System.out.println(add("Hello", "World"));
     }
 
