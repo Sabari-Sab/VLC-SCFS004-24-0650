4ab5d18930bd144f410178d5f27b5f48c38946d8
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-28 11:34:57 +0530

    add constructor overload

---
+++ java samples/src/ClassesAndObjects/constructors/overload/Book.java
@@ -0,0 +1,44 @@
+package classesandobjects.constructors.overload;
+
+public class Book {
+    int bid;
+    String bname;
+    float price;
+
+    // Constructor 1
+    public Book(int bid, String bname) {
+        this.bid = bid;
+        this.bname = bname;
+    }
+
+    // constructor 2
+    public Book(int bid, String bname, float price) {
+        this.bid = bid;
+        this.bname = bname;
+        this.price = price;
+    }
+
+    // constructor 3
+    public Book(String bname, int bid, float price) {
+        this.bid = bid;
+        this.bname = bname;
+        this.price = price;
+    }
+
+    // constructor 4
+    public Book(String bname) {
+        this.bname = bname;
+    }
+
+    public static void main(String[] args) {
+        Book b001 = new Book("MyBook"); // using the constructor 1
+        Book b002 = new Book(100, "Book1"); // using the constructor 2
+        Book b003 = new Book(101, "Good Book", 19.9f); // using the constructor 3 (fully loaded constructor)
+        Book b004 = new Book("SomeBook", 103, 89.9f); // using the constructor 4 (fully loaded constructor)
+
+        // Book one = new Book(); // not allowed as the default constructor is not
+        // present
+
+    }
+
+}
