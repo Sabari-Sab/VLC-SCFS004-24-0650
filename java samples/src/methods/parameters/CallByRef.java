bd8e009e5c65091d29c67fb226712a5a6076e363
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-28 07:38:49 +0530

    add call by ref changes

--- java samples/src/methods/parameters/CallByRef.java
+++ java samples/src/methods/parameters/CallByRef.java
@@ -3,7 +3,6 @@ package methods.parameters;
 public class CallByRef {
     public static void main(String[] args) {
         StringBuilder str = new StringBuilder("Hello");
-        ;
         System.out.println("Before method call is: " + str);
         modifyString(str);
         System.out.println("After method call is: " + str);
@@ -13,4 +12,15 @@ public class CallByRef {
         val.append("World");
         System.out.println("Inside the method the val is: " + val);
     }
+
+    /*
+     * * str is a reference type (StringBuilder).
+     * When the modifyString() method is called and the str argument is passed,
+     * a copy of the reference is made and assigned to the str parameter inside the
+     * method.
+     * Both the original str variable and the str parameter refer to the same
+     * object in memory.
+     * Therefore, modifications made to the str parameter (appending ", World!")
+     * will also affect the original str variable.
+     */
 }
